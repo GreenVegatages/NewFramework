4dd4331639e0a1cf2cacf3e614f7a9bf1910b5e2
GreenVegetable <xuchen1605@gmail.com>
2024-03-04 20:56:46 +0800

    事件中心的帮助类

---
+++ Assets/Scripts/Lvc/Event/IEventHelp.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Lvc.EventModel
+{
+    public interface IEventHelp
+    {
+        
+        protected class EventInfo : IBase
+        {
+            public event UnityAction Actions;
+
+            public EventInfo(UnityAction callback)
+            {
+                Actions = null;
+                Actions += callback;
+            }
+
+            public void Execute()
+            {
+                Actions?.Invoke();
+            }
+        }
+
+        protected class EventInfo<T> : IBase
+        {
+            public event UnityAction<T> Actions;
+
+            public EventInfo(UnityAction<T> callback)
+            {
+                Actions = null;
+                Actions += callback;
+            }
+
+            public void Execute(T value)
+            {
+                Actions?.Invoke(value);
+            }
+        }
+        
+
+    }
+}
